0cda75e9c3f67e71731582a8252570c1a72e5e5e
spoge <notmymail@fake.duh>
2016-03-11 22:38:56 +0100

    JiraIssue0000001: Added rss-library

--- test.go
+++ test.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "github.com/SlyMarbo/rss"
 
 func main() {
-	fmt.Println("hello world")
+	feed, err := rss.Fetch("http://www.nyaa.se/?page=rss&term=shingeki+no+kyojin&offset=1")
+	if err != nil || feed == nil {
+		fmt.Println("Couldn't fetch rss-feed")
+		return
+	}
+
+	// do something
+	fmt.Println(feed.String())
+
+	err = feed.Update()
+	if err != nil {
+		// handle error.
+		fmt.Println(err.Error())
+	}
 }
